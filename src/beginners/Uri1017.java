371600cb7a0194235b8f58666b325eba5bf67ae4
Paulo Matheus <paulomatheusemail@gmail.com>
2021-07-07 14:44:41 -0300

    Enviando resolução do problema URI 1017

---
+++ src/beginners/Uri1017.java
@@ -0,0 +1,32 @@
+package beginners;
+
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Uri1017 {
+    public static void main(String[] args) throws IOException{
+        /*Criando a variável de leitura das informações*/
+        Scanner input = new Scanner(System.in);
+        /*Criando as variáveis para recebimento das informações
+        * e do resultado*/
+        int tempo,vMedia,distancia;
+        float qtdCombustivel;
+
+        /*Pegando as informações e atribuindo as variáveis*/
+        tempo = input.nextInt();
+        vMedia = input.nextInt();
+
+        /*Calculando a distancia percorrida*/
+        distancia = tempo * vMedia;
+
+        /*Agora vamos calcular o quanto de combustível
+        * foi gasto*/
+        /*OBS: É necessário dizer ao JAVA que queremos o resultado
+        * em float pois se trata de dois inteiros. Em uma operação
+        * entre dois inteiros, o JAVA vai deduzir por conta própria
+        * que o resultado seria inteiro*/
+        qtdCombustivel = (float) distancia / 12;
+
+        System.out.printf("%.3f%n",qtdCombustivel);
+    }
+}
