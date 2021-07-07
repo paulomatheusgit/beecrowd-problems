0e1fd0fcd850d5cdf2017b6db52b96e683d7b446
Paulo Matheus <paulomatheusemail@gmail.com>
2021-07-07 12:46:18 -0300

    Enviando resolução do problema URI 1014

---
+++ src/beginners/Uri1014.java
@@ -0,0 +1,25 @@
+package beginners;
+
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Uri1014 {
+    int oi;
+    public static void main(String[] args) throws IOException{
+        /*Criando a vairável de leitura dos dados*/
+        Scanner input = new Scanner(System.in);
+        /*Criando as variável que irão receber os dados*/
+        int km;
+        float l,consumo;
+
+        /*Pegando os valores e atribuindo as variáveis respectivas*/
+        km = input.nextInt();
+        l = input.nextFloat();
+
+        /*calculo do consumo*/
+        consumo = km / l;
+
+        /*Imprimindo o resultado*/
+        System.out.printf("%.3f km/l%n",consumo);
+    }
+}
