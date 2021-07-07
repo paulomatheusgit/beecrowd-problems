6de219749753242ef2915accd902a829998e3120
Paulo Matheus <paulomatheusemail@gmail.com>
2021-07-07 14:28:41 -0300

    Enviando resolução do problema URI 1016

---
+++ src/beginners/Uri1016.java
@@ -0,0 +1,22 @@
+package beginners;
+
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Uri1016 {
+    public static void main(String[] args) throws IOException{
+        /*Criando a variável de leitura dos dados*/
+        Scanner input = new Scanner(System.in);
+        /*Criando a variável de recebimento do dado e a outra
+        * do resultado*/
+        int distancia,minutos;
+
+        /*Lendo a distancia*/
+        distancia = input.nextInt();
+
+        /*Calculando o tempo*/
+        minutos = distancia * 2;
+
+        System.out.println(minutos + " minutos");
+    }
+}
