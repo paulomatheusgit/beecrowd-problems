af5ec1c5d25afc5c1ab720c0d55871f4888a5e0a
Paulo Matheus <paulomatheusemail@gmail.com>
2022-03-04 17:23:16 -0300

    Resolução do problema 1060 da plataforma beecrowd

---
+++ src/beginners/Uri1060.java
@@ -0,0 +1,21 @@
+package beginners;
+
+import java.util.Scanner;
+
+public class Uri1060 {
+    public static void main(String[] args) {
+        float[] numbers = new float[6];
+        int qtdPositives = 0;
+        try (Scanner input = new Scanner(System.in)) {
+            for(int i = 0; i < numbers.length; i++){
+                numbers[i] = input.nextFloat();
+            }
+        }
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] > 0){
+                qtdPositives++;
+            }
+        }
+        System.out.println(qtdPositives + " valores positivos");
+    }
+}
