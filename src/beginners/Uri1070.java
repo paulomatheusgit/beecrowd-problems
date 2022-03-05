5874688fc2d2d8c10b9ddd68aa979ac3e4737cef
Paulo Matheus <paulomatheusemail@gmail.com>
2022-03-05 12:07:41 -0300

    Resolução do problema 1070 da plataforma beecrowd

---
+++ src/beginners/Uri1070.java
@@ -0,0 +1,18 @@
+package beginners;
+
+import java.util.Scanner;
+
+public class Uri1070 {
+    public static void main(String[] args) {
+        int number,qttOdd = 0;
+        Scanner input = new Scanner(System.in);
+        number = input.nextInt();
+        while(qttOdd < 6){
+            if ((number % 2) != 0){
+                qttOdd++;
+                System.out.println(number);
+            }            
+            number++;
+        }
+    }
+}
