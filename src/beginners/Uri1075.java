e2a90e5837e824ba1f628b340499ee9f86c4d5d8
Paulo Matheus <paulomatheusemail@gmail.com>
2022-03-07 10:10:01 -0300

    Resolução do problema 1075 da plataforma beecrowd

---
+++ src/beginners/Uri1075.java
@@ -0,0 +1,17 @@
+package beginners;
+
+import java.util.Scanner;
+
+public class Uri1075 {
+    public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+        
+        n = input.nextInt();
+        for(int i = 1; i <= 10000; i++){
+            if(i % n == 2){
+                System.out.println(i);
+            }
+        }
+    }
+}
