fa23de6ee982e405d8745ffa1367c79cb95fd308
Paulo Matheus <paulomatheusemail@gmail.com>
2022-03-07 10:18:33 -0300

    Resolução do problema 1078 da plataforma beecrowd

---
+++ src/beginners/Uri1078.java
@@ -0,0 +1,15 @@
+package beginners;
+
+import java.util.Scanner;
+
+public class Uri1078 {
+    public static void main(String[] args) {
+        int n;
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i + " x " + n + " = " + (i*n));
+        }
+    }
+}
