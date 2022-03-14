68c3442aaaf44cca1861bf9c04b63a4b8377d5d7
Paulo Matheus <paulomatheusemail@gmail.com>
2022-03-14 18:47:57 -0300

    Resolução do problema 1080 da plataforma beecrowd

---
+++ src/beginners/Uri1080.java
@@ -0,0 +1,19 @@
+package beginners;
+
+import java.util.Scanner;
+
+public class Uri1080 {
+    public static void main(String[] args) {
+        int[] numbers = new int[100];
+        int highest = 0, position = 0;
+        Scanner input = new Scanner(System.in);
+        for(int i = 0; i < numbers.length; i++){
+            numbers[i] = input.nextInt();
+            if(numbers[i] > highest){
+                highest = numbers[i];
+                position = i + 1;
+            }
+        }
+        System.out.println(highest + "\n" + position);
+    }
+}
