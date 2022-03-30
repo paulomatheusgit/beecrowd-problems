8ea39020f56762ff7944485584fc1eec12442acc
Paulo Matheus <paulomatheusemail@gmail.com>
2022-03-30 10:28:28 -0300

    Resolução do problema 1096 da plataforma beecrowd

---
+++ src/beginners/Uri1096.java
@@ -0,0 +1,14 @@
+package beginners;
+
+public class Uri1096 {
+    public static void main(String[] args) {
+        int j; 
+        for (int i = 1; i <= 9; i = i + 2){    
+            j = 7;                 
+            for(int x = 0; x < 3; x++){                
+                System.out.println("I=" + i + " J=" + j);
+                j--;
+            }
+        }
+    }
+}
