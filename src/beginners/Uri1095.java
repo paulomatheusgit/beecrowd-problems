d34df79ac6c8c1274cacbe112b0ec984388bddc5
Paulo Matheus <paulomatheusemail@gmail.com>
2022-03-30 10:06:59 -0300

    Resolução do problema 1095 da plataforma beecrowd

---
+++ src/beginners/Uri1095.java
@@ -0,0 +1,12 @@
+package beginners;
+
+public class Uri1095{
+    public static void main(String[] args) {
+        int i = 1,j = 60;
+        while(j >= 0){
+            System.out.println("I=" + i + " J=" + j );
+            i = i + 3;
+            j = j - 5;
+        }
+    }
+}
