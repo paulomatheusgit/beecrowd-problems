3e4ed3f5dbed4b91046e0d3ae9f062c65df2c8df
Paulo Matheus <paulomatheusemail@gmail.com>
2022-05-03 11:14:18 -0300

    Renomeando o arquivo para o padrão da plataforma (bee)

--- src/beginners/Bee1012.java
+++ src/beginners/Bee1012.java
@@ -3,7 +3,7 @@ package beginners;
 import java.util.Scanner;
 import java.io.IOException;
 
-public class Uri1012 {
+public class Bee1012 {
     public static void main(String[] args) throws IOException{
         /*Criando variável de leitura das informações*/
         Scanner input = new Scanner(System.in);
